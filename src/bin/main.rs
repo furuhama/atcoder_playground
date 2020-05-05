5041cc402db60e349598899c858ff3ab47573619
furuhama <furuhama@users.noreply.github.com>
2020-05-05 14:06:05 +0900

    cargo new

---
+++ src/bin/main.rs
@@ -0,0 +1,14 @@
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Bytes, Chars, Isize1, Usize1},
+};
+
+#[allow(unused_macros)]
+macro_rules! echo {
+    ($($e:expr),+) => ( { $(println!("{}", $e))+ } );
+}
+
+fn main() {
+    unimplemented!();
+}
